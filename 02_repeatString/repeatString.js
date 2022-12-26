2cf52a074bd97e711e50f2ed18afc2144b520023
Nabil <darknab@live.fr>
2022-12-26 12:22:24 +0100

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+if (num === 1) {
+    return string;
+} else if (num === 0){
+    return "";
+} else if (num < 0) {
+    return "ERROR";
+}
 
+let i = 1;
+let retString = string;
+while (i < num ) {
+    
+    retString += string;
+    i++;
+}
+return retString;
 };
 
 // Do not edit below this line
