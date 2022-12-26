036ed97372bed358dc6cfc1693bb4e45027f66b0
Nabil <darknab@live.fr>
2022-12-26 16:37:48 +0100

    Completed excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    if (string.length === 0) {
+        return "";
+    }
+    const letters = [];
+    const revLetters = [];
+    for (i = 0; i < string.length; i++) {
+      letters[i] = string.substr(i,1); 
+      revLetters.unshift(letters[i]); 
+    }
+    
+    
+    return revLetters.join("");
+    }
 
 // Do not edit below this line
 module.exports = reverseString;
