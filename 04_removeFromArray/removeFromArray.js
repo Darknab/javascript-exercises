86e6dd6cabaf58b7cf6c67af625a6d9d0237a3d7
Nabil <darknab@live.fr>
2022-12-27 11:42:15 +0100

    Completed exersise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(items) {
+    //first argument is an array
+    const arr = arguments[0];
+    //if there is only one argument, don't remove anytning, return the array untouched
+    switch (arguments.length) {
+        case 0 :
+        return "";
+        case 1: 
+        return arr;
+        //make a loop that select items one by one and remove the actual item from the array each time
+        default: 
+        for (let j = 1; j < arguments.length; j++) {
+            for (let i = 0; i < arr.length; i++) {
+                let item = arr[i];
+                if (item === arguments[j]) {
+                    arr.splice(i, 1);   
+                } 
+            }            
+        }
+        return arr;
+    }
 };
 
 // Do not edit below this line
