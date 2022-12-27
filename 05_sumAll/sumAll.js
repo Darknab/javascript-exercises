8e1794694d1d39f81e231707d74dbd93162771d5
Nabil <darknab@live.fr>
2022-12-27 17:11:34 +0100

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
 const sumAll = function() {
+    //make a function that makes simply the sum
+    const sum = function(num1,num2) {
+        let sum = num1;
+        for (i = (num1 + 1); i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    //switch for the diffrent  cases possible
+    if (arguments[0] > arguments[1]) {
+        return sum(arguments[1],arguments[0]);
+    } else {
+        if (arguments[0] < 0 || arguments[1] < 0) {
+            return "ERROR";
+        } else {
+            if (typeof arguments[0] !== "number" || typeof arguments[1] !== "number") {
+                return "ERROR";
+            } else {
+                return sum(arguments[0],arguments[1]);
+            }
+        }
+    }
 
 };
 
