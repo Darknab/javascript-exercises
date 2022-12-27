7bc5400d52216f5a2c31fbbda530643547990e4e
Nabil <darknab@live.fr>
2022-12-27 18:03:44 +0100

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const rounded = function(num) {
+  return (Math.round(num * 10) / 10)
+}
+
+
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
+  return rounded(tempC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * (9 / 5) + 32);
+  return rounded(tempF);
 };
 
 // Do not edit below this line
