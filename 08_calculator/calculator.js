52caae5b80df4f995f81e5ce2137516f7400b09c
Nabil <darknab@live.fr>
2022-12-27 18:29:44 +0100

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  for (number of numbers) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let mult = 1;
+  for (number of numbers) {
+    mult *= number;
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let power = 1;
+  for (let i = 1 ; i <= b; i++) {
+    power *= a;
+  }
+  return power;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let fact = 1;
+  for (let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 	
 };
 
