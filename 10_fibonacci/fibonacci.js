7f236f94a1fcdfa5747897c6b5b7d9451575d337
Nabil <darknab@live.fr>
2022-12-28 19:02:36 +0100

    Completed excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num <= 0 ) {
+        return "OOPS"
+    }
+    const elements = [0,0,1];
+    for (i = 2; i <= num; i++) {
+        elements[i + 1] = elements[i] + elements[i - 1];
 
+    }
+return elements[num + 1];
 };
 
 // Do not edit below this line
