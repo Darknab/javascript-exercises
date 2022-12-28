5f61198f655343688fb88eab14ed8d1e924ace0a
Nabil <darknab@live.fr>
2022-12-28 12:27:30 +0100

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,43 @@
-const palindromes = function () {
+const removeFromArray = function(items) {
+    //first argument is an array
+    const arr = arguments[0];
+    //if there is only one argument, don't remove anytning, return the array untouched
+    switch (arguments.length) {
+        case 0 :
+        return "";
+        case 1: 
+        return arr;
+        //make a loop that select items one by one and remove the actual item from the array each time
+        default: 
+        for (let j = 1; j < arguments.length; j++) {
+            for (let i = 0; i < arr.length; i++) {
+                let item = arr[i];
+                if (item === arguments[j]) {
+                    arr.splice(i, 1);   
+                } 
+            }            
+        }
+        return arr;
+    }
+};
 
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    // transform the string to an array and reverse it
+    const letters = [];
+    const revLetters = [];
+    for (i = 0; i < text.length; i++) {
+      letters[i] = text.substr(i,1); 
+      revLetters.unshift(letters[i]); 
+    }
+    // remove ponctuations and spaces
+    removeFromArray(letters," ",",","!","?",".");
+    removeFromArray(revLetters," ",",","!","?",".");
+
+    if (letters.join("") === revLetters.join("")) {
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
