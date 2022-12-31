b0735d5276f64b808d18cabaaa350a47ac3c3c20
Nabil <darknab@live.fr>
2022-12-31 10:08:56 +0100

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    const age = function(person) {
+        const date = new Date()
+        const actualYear = date.getFullYear();
+        if (person.yearOfDeath === undefined) {
+            return  (actualYear - person.yearOfBirth);
+        }
+        else return  (person.yearOfDeath - person.yearOfBirth);
+    }
+    let oldest = persons[0]
+    for (element of persons) {
+         element.age = age(element);
+         console.log(element.age);
+         if (element.age > oldest.age) {
+            oldest = element;
+         }
+    }
+return oldest
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
